1eb631b640cab2050aa22c14b858fdde7a573499
oh-ji-yeon <o.delayed0106@gmail.com>
2025-04-16 09:59:54 +0900

    cookie

--- ch2/src/main/java/com/fastcampus/ch2/LoginController.java
+++ ch2/src/main/java/com/fastcampus/ch2/LoginController.java
@@ -2,6 +2,9 @@ package com.fastcampus.ch2;
 
 import java.net.URLEncoder;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +19,10 @@ public class LoginController {
 	}
 
 	@PostMapping("/login")
-	public String login(String id, String pwd, String rememberId) throws Exception {
+	public String login(String id, String pwd, boolean rememberId, HttpServletResponse response) throws Exception {
+		System.out.println("id= " + id);
+		System.out.println("pwd= " + pwd);
+		System.out.println("remeberId= " + rememberId);
 		// 1. id와 pwd를 확인
 		if (!loginCheck(id, pwd)) {
 			// 2-1. 일치하지 않으면, loginForm으로 이동
@@ -24,7 +30,18 @@ public class LoginController {
 			
 			return "redirect:/login/login?msg=" + msg;
 		}
-		// 2-2. 일치하면, 홈으로 이동
+		// 2-2. 일치하면,
+		if(rememberId) {
+			// 쿠키를 생성
+			Cookie cookie = new Cookie("id", id);
+			response.addCookie(cookie);
+		} else {
+			// 쿠키를 삭제
+			Cookie cookie = new Cookie("id", id);
+			cookie.setMaxAge(0);
+			response.addCookie(cookie);
+		}
+			// 홈으로 이동
 		return "redirect:/";
 	}
 
